8b1bb8a06764df3edb924ce5721db49e97a9438b
Joel Quiles <quilesbaker@gmail.com>
2018-05-06 21:04:58 -0400

    Update and rename create-links.sh to install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ do
         if [ ! -f ~/$file ]; then
             echo Creating symlink for" $file
             ln -s "$(readlink -e "$file")" ~/$file
-		    fi
-	  fi
+        fi
+    fi
 done
+
+echo Installing oh-my-zsh...
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
